3744a9bca64671a3745d9fa76d9ec16b44753df8
rapiddeveloper22 <mmaswin22@gmail.com>
2022-02-25 07:24:57 +0530

    first commit

--- nodejs.js
+++ nodejs.js
@@ -3,24 +3,25 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const fileUpload = require('express-fileupload');
 const fs = require('fs');
+const fleekStorage = require('@fleekhq/fleek-storage-js')
 const pinataSDK = require('@pinata/sdk');
 
 const ipfs = ipfsClient.create({ host: 'localhost', port: '5001', protocol: 'http' });
 const app = express();
 const pinata = pinataSDK('df7bf7e4d65592982531', 'ccc347d16afbf1fbf1a424af537d4b0152687e040d8ded3bbeff800087839765');
 
-const options = {
-    pinataMetadata: {
-        name: 'My Awesome Website',
-        keyvalues: {
-            customKey: 'customValue',
-            customKey2: 'customValue2'
-        }
-    },
-    pinataOptions: {
-        cidVersion: 0
-    }
-};
+// const options = {
+//     pinataMetadata: {
+//         name: 'My Awesome Website',
+//         keyvalues: {
+//             customKey: 'customValue',
+//             customKey2: 'customValue2'
+//         }
+//     },
+//     pinataOptions: {
+//         cidVersion: 0
+//     }
+// };
 
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -30,13 +31,13 @@ app.get('/', (req, res) => {
     res.render('home');
 });
 
-pinata.testAuthentication().then((result) => {
-    //handle successful authentication here
-    console.log(result);
-}).catch((err) => {
-    //handle error here
-    console.log(err);
-});
+// pinata.testAuthentication().then((result) => {
+//     //handle successful authentication here
+//     console.log(result);
+// }).catch((err) => {
+//     //handle error here
+//     console.log(err);
+// });
 
 app.post('/upload', async (req, res) => {
     const file = req.files.file;
@@ -46,15 +47,35 @@ app.post('/upload', async (req, res) => {
     let fullPath = req.body.path;
     fullPath = fullPath + "\\" + file.name;
     console.log(fullPath);
+    let filePath = fullPath;
 
-    pinata.pinFromFS(fullPath, options).then((result) => {
+
+    fs.readFile(filePath, async (error, fileData) => {
+        const uploadedFile = await fleekStorage.upload({
+            apiKey: 'ML41Qz4GTgN0avpiJ0whww==',
+            apiSecret: 'lXSFV61xUALbLpKRFZN2dVRnYJiA15i0HUQ8GYXuA6M=',
+            key: file.name,
+            ContentType: 'pdf',
+            data: fileData,
+            httpUploadProgressCallback: (event) => {
+                console.log(Math.round(event.loaded / event.total * 100) + '% done');
+            }
+        });
         fileName = file.name;
-        fileHash = result.IpfsHash;
-        console.log(result);
+        fileHash = uploadedFile.hashV0;
+        console.log(uploadedFile);
+
         res.render('upload', { fileName, fileHash });
-    }).catch((err) => {
-        console.log(err);
-    });
+    })
+
+    // pinata.pinFromFS(fullPath, options).then((result) => {
+    //     fileName = file.name;
+    //     fileHash = result.IpfsHash;
+    //     console.log(result);
+    //     res.render('upload', { fileName, fileHash });
+    // }).catch((err) => {
+    //     console.log(err);
+    // });
 
 
 
